e105e396a0f5db555205b2fa493dfaf42988f458
h0pped <notawril@gmail.com>
2021-09-04 19:23:09 +0300

    changed palette

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,5 @@
 const cardContainer = document.querySelector("#generate-card-container");
+const body = document.querySelector("body");
 const card = document.querySelector("#card");
 
 let cardObserverOptions = {
@@ -10,11 +11,15 @@ function cardObserver(entries, observer) {
   console.log(...entries);
   if (entries[0].isIntersecting) {
     console.log("TRYE");
+    body.style.backgroundColor = "#b99c85";
     card.classList.remove("hidden");
   } else {
     card.classList.add("hidden");
+    body.style.backgroundColor = "#CCB3A3";
   }
 }
 let observer = new IntersectionObserver(cardObserver, cardObserverOptions);
 
 observer.observe(cardContainer);
+
+// background: colors.$card-page-color;
