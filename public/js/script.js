13651ab9e37ab4247f9e5f50002299c4d19ab655
h0pped <notawril@gmail.com>
2021-09-05 13:28:40 +0300

    fix: correct animation work and margins

--- public/js/script.js
+++ public/js/script.js
@@ -1,30 +1,55 @@
 const cardContainer = document.querySelector("#generate-card-container");
+const titleContainer = document.querySelector("#title-container");
 const titleWrapper = document.querySelector(".title-wrap");
+const readyCardContainer = document.querySelector("#ready-card-container");
 const body = document.querySelector("body");
 const card = document.querySelector("#card");
-
+const submitBtn = document.querySelector(".submit-btn");
 let cardObserverOptions = {
   root: null,
   rootMargin: "0px",
   threshold: 0.2,
 };
+let titleObserverOptions = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.5,
+};
 function cardObserver(entries, observer) {
   console.log(...entries);
   if (entries[0].isIntersecting) {
-    console.log("TRYE");
     body.style.backgroundColor = "#b99c85";
     card.classList.remove("hidden");
-    titleWrapper.classList.add("hidden");
   } else {
     card.classList.add("hidden");
-    titleWrapper.classList.remove("hidden");
-
-    body.style.backgroundColor = "#CCB3A3";
   }
 }
-
+function titleObserverFunc(entries, observer) {
+  if (entries[0].isIntersecting) {
+    titleWrapper.classList.remove("hidden");
+    body.style.backgroundColor = "#CCB3A3";
+  } else {
+    titleWrapper.classList.add("hidden");
+  }
+}
 let observer = new IntersectionObserver(cardObserver, cardObserverOptions);
+let titleObserver = new IntersectionObserver(
+  titleObserverFunc,
+  titleObserverOptions
+);
 
 observer.observe(cardContainer);
+titleObserver.observe(titleContainer);
+
+submitBtn.addEventListener("click", (e) => {
+  showReadyCardContainer();
+  readyCardContainer.scrollIntoView({
+    behavior: "smooth",
+  });
+});
+
+function showReadyCardContainer() {
+  readyCardContainer.classList.remove("hidden");
+}
 
 // background: colors.$card-page-color;
