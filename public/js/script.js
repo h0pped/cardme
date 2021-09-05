0fc5062f1788af749402de3d9867d1933cfd991e
h0pped <notawril@gmail.com>
2021-09-05 10:37:53 +0300

    feat: add animation on scroll

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,12 @@
 const cardContainer = document.querySelector("#generate-card-container");
+const titleWrapper = document.querySelector(".title-wrap");
 const body = document.querySelector("body");
 const card = document.querySelector("#card");
 
 let cardObserverOptions = {
   root: null,
   rootMargin: "0px",
-  threshold: 0.6,
+  threshold: 0.2,
 };
 function cardObserver(entries, observer) {
   console.log(...entries);
@@ -13,11 +14,15 @@ function cardObserver(entries, observer) {
     console.log("TRYE");
     body.style.backgroundColor = "#b99c85";
     card.classList.remove("hidden");
+    titleWrapper.classList.add("hidden");
   } else {
     card.classList.add("hidden");
+    titleWrapper.classList.remove("hidden");
+
     body.style.backgroundColor = "#CCB3A3";
   }
 }
+
 let observer = new IntersectionObserver(cardObserver, cardObserverOptions);
 
 observer.observe(cardContainer);
