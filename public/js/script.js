021724111b13703f0fa21572ef2db254af924b85
h0pped <notawril@gmail.com>
2021-09-06 12:50:48 +0300

    feat: generate new card, ui for created card

--- public/js/script.js
+++ public/js/script.js
@@ -13,8 +13,16 @@ const companyInput = document.querySelector("#companyInput");
 const descriptionInput = document.querySelector("#descriptionInput");
 const emailInput = document.querySelector("#emailInput");
 const numberInput = document.querySelector("#numberInput");
-
 const description = document.querySelector("#description");
+
+var QR_CODE = new QRCode("qrcode", {
+  width: 220,
+  height: 220,
+  colorDark: "#000000",
+  colorLight: "transparent",
+  correctLevel: QRCode.CorrectLevel.H,
+});
+
 let cardObserverOptions = {
   root: null,
   rootMargin: "0px",
@@ -83,7 +91,6 @@ const generateCardBody = function () {
 const sendCard = () =>
   new Promise((resolve, reject) => {
     let card = generateCardBody();
-    console.log(card);
     fetch("/", {
       method: "POST",
       headers: {
@@ -92,24 +99,163 @@ const sendCard = () =>
       body: JSON.stringify(card),
     })
       .then((res) => res.json())
-      .then((data) => resolve(data))
+      .then((data) => (data.err ? reject(data.err) : resolve(data)))
       .catch((err) => {
         reject(err);
       });
   });
+const generateDescriptionBlocks = (description) => {
+  let descriptionBlocks = "";
 
+  description.split("\n").forEach((el) => {
+    descriptionBlocks += `
+      <div class="card-block">
+      <div class="card-block-content">
+        <h3>${el}</h3>
+      </div>
+    </div>
+      `;
+  });
+  return descriptionBlocks;
+};
+const createResultCard = (data) => {
+  let cardEl = document.createElement("div");
+  cardEl.classList.add("card");
+
+  //NAME BLOCK
+  cardEl.innerHTML += `
+  <div class="card-block">
+    <div class="card-block-content">
+      <h3>Hello!</h3>
+    </div>
+  </div>
+  <div class="card-block">
+    <div class="card-block-content">
+      <h3>My name is ${data.name}</h3>
+    </div>
+  </div>
+  `;
+
+  // JOB BLOCK
+  if (data.job)
+    cardEl.innerHTML += `
+  <div class="card-block">
+    <div class="card-block-content">
+      <h3>I am ${data.job} ${
+      data.company_name.length != 0 ? `in ${data.company_name}` : ""
+    }</h3>
+    </div>
+  </div>
+  `;
+
+  //DESCRIPTION BLOCKS
+  if (data.description)
+    cardEl.innerHTML += generateDescriptionBlocks(data.description);
+
+  //email
+  if (data.email)
+    cardEl.innerHTML += `
+  <div class="card-block block-stretch">
+    <div class="card-block-content">
+      <h3>My Email:
+        <a
+          href="mailto:${data.email}"
+        >${data.email}</a></h3>
+    </div>
+  </div>
+  `;
+  if (data.phone)
+    cardEl.innerHTML += `
+  <div class="card-block">
+    <div class="card-block-content">
+      <h3>Phone number:
+        <a href="tel:${data.phone}">${data.phone}</a></h3>
+
+    </div>
+  </div>
+  `;
+  return cardEl;
+  /*
+   <div id="ready-card-container" class="hidden">
+        <div class="left">
+
+          <div class="card">
+            <div class="card-block">
+              <div class="card-block-content">
+                <h3>Hello!</h3>
+              </div>
+            </div>
+            <div class="card-block">
+              <div class="card-block-content">
+                <h3>My name is Illia</h3>
+              </div>
+            </div>
+            <div class="card-block">
+              <div class="card-block-content">
+                <h3>I am Digital Assistant At Google</h3>
+              </div>
+
+            </div>
+            <div class="card-block">
+              <div class="card-block-content">
+                <h3>I help people to manage their tasks in a specific way</h3>
+              </div>
+            </div>
+            <div class="card-block">
+              <div class="card-block-content">
+                <h3>
+                  Also I like dogs :)
+                </h3>
+              </div>
+            </div>
+            <div class="card-block block-stretch">
+              <div class="card-block-content">
+                <h3>My Email:
+                  <a
+                    href="mailto:notawril@gmail.com"
+                  >notawril@gmail.com</a></h3>
+              </div>
+            </div>
+            <div class="card-block">
+              <div class="card-block-content">
+                <h3>Phone number:
+                  <a href="tel:+380958539914">+380958539114</a></h3>
+
+              </div>
+            </div>
+
+          </div>
+          <div id="add-links-container">
+            <button id="add-link-btn" class="links-button">
+              Add link
+            </button>
+            <button id="add-telegram-btn" class="links-button">
+              Add Telegram
+            </button>
+            <button id="add-whatsapp-btn" class="links-button">
+              Add WhatsApp
+            </button>
+          </div>
+        </div>*/
+};
 submitBtn.addEventListener("click", async (e) => {
   console.log("AAA");
   sendCard()
-    .then((result) => console.log(result))
+    .then((result) => {
+      QR_CODE.makeCode(document.URL + result._id);
+      readyCardContainer.querySelector(".left").innerHTML = "";
+      readyCardContainer
+        .querySelector(".left")
+        .appendChild(createResultCard(result));
+      showReadyCardContainer();
+    })
     .catch((err) => {
       console.log(err);
     });
-  showReadyCardContainer();
-  readyCardContainer.scrollIntoView({
-    behavior: "smooth",
-  });
 });
 function showReadyCardContainer() {
   readyCardContainer.classList.remove("hidden");
+  readyCardContainer.scrollIntoView({
+    behavior: "smooth",
+  });
 }
